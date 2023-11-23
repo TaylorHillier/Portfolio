c6bb6fd399e18d16c7213b56fc301ecad84f8696
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2023-11-23 14:15:33 -0800

    code optimization and some aria's added

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -7,28 +7,27 @@ function AboutPage({ apiUrl }) {
   const [currentKeywordIndex, setCurrentKeywordIndex] = useState(0);
   const [isLoaded, setLoadStatus] = useState(false);
 
-
   const keywords = [
     aboutFields.acf && aboutFields.acf.about_keyword_group && aboutFields.acf.about_keyword_group.about_keyword_1,
     aboutFields.acf && aboutFields.acf.about_keyword_group && aboutFields.acf.about_keyword_group.about_keyword_2,
     aboutFields.acf && aboutFields.acf.about_keyword_group && aboutFields.acf.about_keyword_group.about_keyword_3,
   ];
-  
+
   useEffect(() => {
     const fetchData = async () => {
-        const response = await fetch(`${apiUrl}pages/38?acf_format=standard`, {
-          headers: {
-            Authorization:
-              'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0Ojg4ODgvcG9ydGZvbGlvIiwiaWF0IjoxNzAwMjUwMTAyLCJuYmYiOjE3MDAyNTAxMDIsImV4cCI6MTcwMDg1NDkwMiwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMSJ9fX0.n7YwwJRY-3KJ725uHmouA2_fHj8GBx2LOi16yKtuP_8',
-          }})
-          if(response.ok){
-            const data = await response.json();
-            setAboutFields(data);
-            setLoadStatus(true);
-          } else {
-            setLoadStatus(false);
-          }
-
+      const response = await fetch(`${apiUrl}pages/38?acf_format=standard`, {
+        headers: {
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0Ojg4ODgvcG9ydGZvbGlvIiwiaWF0IjoxNzAwMjUwMTAyLCJuYmYiOjE3MDAyNTAxMDIsImV4cCI6MTcwMDg1NDkwMiwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMSJ9fX0.n7YwwJRY-3KJ725uHmouA2_fHj8GBx2LOi16yKtuP_8',
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setAboutFields(data);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
     };
     fetchData();
   }, [apiUrl]);
@@ -54,59 +53,64 @@ function AboutPage({ apiUrl }) {
   UseReveal();
 
   return (
-      <>
-      { isLoaded && 
-      <section className='relative z-10 bg-gradient-to-b from-[#151d1f] via-[#0a0a19] to-[#70828F] text-white w-full'>
-        <div className='md:flex'>
-        <img src={aboutFields.acf && aboutFields.acf.portrait} loading="lazy" alt="Photo of myself - Taylor Hillier" width='768px' height='100%' className='max-h-screen max-w-full object-cover object-center md:w-1/2'/>
-        <div className='about p-4 h-[80vh] reveal fade-bottom md:px-[5vh]' id="about">
-          <h1 className='border-solid border border-gray-400 max-w-fit p-2 my-8 shine'>
-            ABOUT
-          </h1>
-          <div id='aboutKeywordAnimation' className='h-8 ' aria-live="polite">
-            <TypeAnimation
-            sequence={[
-              keywords[0],
-              4000, 
-              keywords[1],
-              4000,
-              keywords[2],
-              4000
-            ]}
-            wrapper="span"
-            speed={30}
-            style={{ fontSize: '2em', display: 'inline-block' }}
-            repeat={Infinity}
+    <>
+      {isLoaded && (
+        <section className="relative z-10 bg-gradient-to-b from-[#151d1f] via-[#0a0a19] to-[#70828F] text-white w-full">
+          <div className="md:flex md:flex-row-reverse">
+            <img
+              src={aboutFields.acf && aboutFields.acf.portrait}
+              loading="lazy"
+              alt="Photo of myself - Taylor Hillier"
+              width="768px"
+              height="100%"
+              className="max-h-screen max-w-full object-cover object-center md:w-1/2"
             />
-          </div>
-          <div className='aboutParagraph my-8 reveal fade-bottom'>
-            <PlainTextToHTML plainText={aboutFields.acf && aboutFields.acf.about_me_paragraph} />
-          </div>
-          <div className='aboutMeSkills py-4'>
-            <p className='font-bold reveal fade-bottom'>Some skills I have developed:</p>
-            <ul className='skills rounded shadow-lg shadow-gray-400 p-2 mt-4  grid grid-cols-3 justify-items-center reveal fade-bottom'>
-              {aboutFields.acf &&
-                aboutFields.acf.skill_repeater &&
-                aboutFields.acf.skill_repeater.map((skill, index) => (
-                  <li key={index} className='p-2'>
-                    {skill.skill_group.skill_image && (
-                      <img
-                        src={skill.skill_group.skill_image}
-                        alt={skill.skill_group.skill_label || `Skill ${index + 1}`}
-                      />
-                    )}
-                  </li>
-                ))}
-            </ul>
-          </div>
-        </div>
-        </div>
-        <div className='h-px w-full bg-gradient-to-r from-transparent via-white to-transparent'>
-              <div className='h-full bg-gradient-to-r from-neutrals-100/30 via-neutrals-100 to-neutrals-100/30'></div>
+            <div className="about p-4 h-[80vh] reveal fade-bottom md:px-[5vh]" id="about">
+              <h1 className="border-solid border border-gray-400 max-w-fit p-2 my-8 shine">ABOUT</h1>
+              <div id="aboutKeywordAnimation" className="h-8 lg:text-3xl lg:my-16" aria-live="polite">
+                <TypeAnimation
+                  sequence={[
+                    keywords[0],
+                    4000,
+                    keywords[1],
+                    4000,
+                    keywords[2],
+                    4000,
+                  ]}
+                  wrapper="span"
+                  speed={30}
+                  style={{ fontSize: '2em', display: 'inline-block' }}
+                  repeat={Infinity}
+                />
+              </div>
+              <div className="aboutParagraph my-8 reveal fade-bottom">
+                <PlainTextToHTML plainText={aboutFields.acf && aboutFields.acf.about_me_paragraph} />
+              </div>
+              <div className="aboutMeSkills py-4">
+                <p className="font-bold reveal fade-bottom">Some skills I have developed:</p>
+                <ul className="skills rounded shadow-lg shadow-gray-400 p-2 mt-4 grid grid-cols-3 justify-items-center reveal fade-bottom">
+                  {aboutFields.acf &&
+                    aboutFields.acf.skill_repeater &&
+                    aboutFields.acf.skill_repeater.map((skill, index) => (
+                      <li key={index} className="p-2">
+                        {skill.skill_group.skill_image && (
+                          <img
+                            src={skill.skill_group.skill_image}
+                            alt={skill.skill_group.skill_label || `Skill ${index + 1}`}
+                          />
+                        )}
+                      </li>
+                    ))}
+                </ul>
+              </div>
             </div>
-      </section>
-      }
-      </>
+          </div>
+          <div className="h-px w-full bg-gradient-to-r from-transparent via-white to-transparent">
+            <div className="h-full bg-gradient-to-r from-neutrals-100/30 via-neutrals-100 to-neutrals-100/30"></div>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
 
--- frontend/src/Components/Projects.js
+++ frontend/src/Components/Projects.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import UseReveal from './Reveal';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function ProjectData({ apiUrl }) {
   const [projects, setProjects] = useState([]);
@@ -8,23 +8,22 @@ function ProjectData({ apiUrl }) {
 
   useEffect(() => {
     const fetchData = async () => {
-        const response = await fetch(`${apiUrl}project?_embed`, {
-          headers: {
-            Authorization:
-              'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0Ojg4ODgvcG9ydGZvbGlvIiwiaWF0IjoxNzAwMjUwMTAyLCJuYmYiOjE3MDAyNTAxMDIsImV4cCI6MTcwMDg1NDkwMiwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMSJ9fX0.n7YwwJRY-3KJ725uHmouA2_fHj8GBx2LOi16yKtuP_8',
-          }})
-          if(response.ok){
-            const data = await response.json();
-            setProjects(data);
-            setLoadStatus(true);
-          } else {
-            setLoadStatus(false);
-          }
-
+      const response = await fetch(`${apiUrl}project?_embed`, {
+        headers: {
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0Ojg4ODgvcG9ydGZvbGlvIiwiaWF0IjoxNzAwMjUwMTAyLCJuYmYiOjE3MDAyNTAxMDIsImV4cCI6MTcwMDg1NDkwMiwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMSJ9fX0.n7YwwJRY-3KJ725uHmouA2_fHj8GBx2LOi16yKtuP_8',
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setProjects(data);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
     };
     fetchData();
   }, [apiUrl]);
-
 
   const TabContent = ({ project }) => {
     const [activeTab, setActiveTab] = useState('summary');
@@ -48,10 +47,10 @@ function ProjectData({ apiUrl }) {
     UseReveal();
 
     return (
-      <div className='tab-container my-4'>
-        <div className='tabs'>
+      <div className="tab-container my-4">
+        <div className="tabs">
           <div
-            className={`tab ${activeTab === 'summary' ? 'active' : ''}  hover:scale-105 hover:text-white`}
+            className={`tab ${activeTab === 'summary' ? 'active' : ''} hover:scale-105 hover:text-white`}
             onClick={() => setActiveTab('summary')}
           >
             <button>Summary</button>
@@ -65,24 +64,24 @@ function ProjectData({ apiUrl }) {
           </div>
         </div>
 
-        <div className='tab-content-container md:w-[40vw]'>
-          <div className={`tab-content  ${activeTab === 'summary' ? 'active' : ''}`}>
+        <div className="tab-content-container md:w-[40vw]">
+          <div className={`tab-content ${activeTab === 'summary' ? 'active' : ''}`}>
             {showFullSummary ? (
-              <div className='backdrop-brightness-90 p-4 shadow-lg shadow-slate-400'>
+              <div className="backdrop-brightness-90 p-4 shadow-lg shadow-slate-400">
                 <PlainTextToHTML plainText={project.acf.project_summary} />
                 <button
                   onClick={toggleSummary}
-                  className='flex text-sm m-auto border-solid border p-2 mt-4 bg-white text-[#151d1f] font-bold hover:scale-105'
+                  className="flex text-sm m-auto border-solid border p-2 mt-4 bg-white text-[#151d1f] font-bold hover:scale-105"
                 >
                   Read Less
                 </button>
               </div>
             ) : (
-              <div className='backdrop-brightness-90 p-4 shadow-lg shadow-slate-400 md:flex md:flex-col md:justify-between md:min-h-[30vh]'>
+              <div className="backdrop-brightness-90 p-4 shadow-lg shadow-slate-400 md:flex md:flex-col md:justify-between md:min-h-[30vh]">
                 <PlainTextToHTML plainText={project.acf.project_summary.substring(0, 250) + '....'} />
                 <button
                   onClick={toggleSummary}
-                  className='flex text-sm mx-auto border-solid border p-2 mt-4 bg-white text-[#151d1f] font-bold hover:scale-105'
+                  className="flex text-sm mx-auto border-solid border p-2 mt-4 bg-white text-[#151d1f] font-bold hover:scale-105"
                 >
                   Read More
                 </button>
@@ -102,36 +101,54 @@ function ProjectData({ apiUrl }) {
 
   return (
     <>
-    { isLoaded && 
-      <section className='relative projects z-10 bg-gradient-to-t from-[#0a0a19] via-[#151d1f] to-[#70828F] p-2 md:p-[5vw] md:pt-[15vw] md:mb-0 m' id='projects'>
-        <h1 className='border-solid border max-w-fit p-2 m-auto font-thin shine'>PROJECTS</h1>
-        <div className='projectArticles md:grid md:grid-cols-2 md:gap-[5vw] mt-[10vh]'>
-        {projects.map((project) => (
-            <article key={project.id} className='my-16 reveal fade-bottom  md:m-auto md:items-top md:my-0' id={project.slug}>
-                <h2 className=' font-bold text-3xl mb-4 md:min-h-[8vh]'>{project.title.rendered}</h2>
-                <div className='flex items-center gap-4 my-4 '>
-                  {project.acf.github_link != "" &&
-                    <a href={project.acf.github_link} className='flex gap-1 hover:scale-105 hover:text-white hover:underline hover:font-semibold'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill='white' viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
-                    <p>Github Repo</p>
-                  </a>
-                    }
-                  {project.slug != 'portfolio' &&
-                    <Link to={`/project/${project.slug}`} className='flex gap-1 hover:scale-105 hover:text-white hover:underline hover:font-semibold'>
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill='white' fill-rule="evenodd" clip-rule="evenodd"><path d="M14 4h-13v18h20v-11h1v12h-22v-20h14v1zm10 5h-1v-6.293l-11.646 11.647-.708-.708 11.647-11.646h-6.293v-1h8v8z"/></svg>
-                        <p>Project Gallery</p>
+      {isLoaded && (
+        <section className="relative projects z-10 bg-gradient-to-t pt-[10vw] from-[#0a0a19] via-[#151d1f] to-[#70828F] p-4 md:p-[5vw] md:pt-[10vw] md:mb-0 " id="projects">
+          <h1 className="border-solid border max-w-fit p-2 m-auto lg:mx-4 font-thin shine">PROJECTS</h1>
+          <h2 className="text-4xl mt-[10vh] lg:mx-4 reveal fade-bottom mx-auto text-center">Explore the work I've done (so far).</h2>
+          <div className="projectArticles md:grid md:grid-cols-2 md:gap-[5vw] mt-[10vh]">
+            {projects.map((project) => (
+              <article key={project.id} className="my-16 reveal fade-bottom md:m-auto md:items-top md:my-0" id={project.slug}>
+                <h2 className="font-bold text-3xl mb-4 md:min-h-[8vh]">{project.title.rendered}</h2>
+                <div className="flex items-center gap-4 my-4">
+                  {project.acf.github_link !== "" && (
+                    <a
+                      href={project.acf.github_link}
+                      className="flex gap-1 hover:scale-105 hover:text-white hover:underline hover:font-semibold"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
+                        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                      </svg>
+                      <p>Github Repo</p>
+                    </a>
+                  )}
+                  {project.slug !== 'portfolio' && (
+                    <Link
+                      to={`/project/${project.slug}`}
+                      className="flex gap-1 hover:scale-105 hover:text-white hover:underline hover:font-semibold"
+                    >
+                      <svg
+                        width="24"
+                        height="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="white"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                      >
+                        <path d="M14 4h-13v18h20v-11h1v12h-22v-20h14v1zm10 5h-1v-6.293l-11.646 11.647-.708-.708 11.647-11.646h-6.293v-1h8v8z" />
+                      </svg>
+                      <p>Project Gallery</p>
                     </Link>
-                  } 
-              </div>
-              <div className='h-px w-full bg-gradient-to-r from-transparent via-white to-transparent'>
-                <div className='h-full bg-gradient-to-r from-neutrals-100/30 via-neutrals-100 to-neutrals-100/30'></div>
-              </div>
-              <TabContent key={project.id} project={project} />
-            </article>
-        ))}
-        </div>
-      </section>
-    }
+                  )}
+                </div>
+                <div className="h-px w-full bg-gradient-to-r from-transparent via-white to-transparent">
+                  <div className="h-full bg-gradient-to-r from-neutrals-100/30 via-neutrals-100 to-neutrals-100/30"></div>
+                </div>
+                <TabContent key={project.id} project={project} />
+              </article>
+            ))}
+          </div>
+        </section>
+      )}
     </>
   );
 }
