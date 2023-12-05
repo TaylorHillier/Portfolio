b09c3ca2152693efbeb6c08dcc28ce215e853a4f
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2023-12-04 20:03:16 -0800

    build

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -27,7 +27,8 @@ function Nav() {
       } ${isGalleryPage ? 'hidden' : ''}`} 
       id='top-nav'
     >
-      <ul className='md:flex md:justify-evenly text-white grid grid-cols-3 justify-items-center'>
+      <ul className='md:flex md:justify-evenly text-white grid grid-cols-4 justify-items-center'>
+        <NavItem to="/#top-of-content" icon="home" label="Home" />
         <NavItem to="/#about" icon="about" label="About" />
         <NavItem to="/#projects" icon="projects" label="Projects" />
         <NavItem to="/#contact" icon="contact" label="Contact" />
@@ -40,18 +41,33 @@ function Nav() {
 }
 
 function NavItem({ to, icon, label }) {
+  if(to !== '/#top-of-content') {
+    return (
+      <li className='p-4 text-sm font-thin w-full hover:scale-110 lg:hover:scale-100 hover:bg-[#0a0a19] lg:hover:bg-transparent'>
+        <a href={to} className='flex-col flex items-center justify-items-center scroll-to lg:hover:font-bold lg:hover:scale-110'  aria-label={label}>
+          {renderIcon(icon)}
+          {label}
+        </a>
+      </li>
+    );
+  } else {
   return (
     <li className='p-4 text-sm font-thin w-full hover:scale-110 lg:hover:scale-100 hover:bg-[#0a0a19] lg:hover:bg-transparent'>
-      <a href={to} className='flex-col flex items-center justify-items-center scroll-to lg:hover:font-bold lg:hover:scale-110'  aria-label={label}>
+      <a href={to} className='flex-col flex items-center justify-items-center scroll-to lg:hover:font-bold lg:hover:scale-110 lg:hidden'  aria-label={label}>
         {renderIcon(icon)}
         {label}
       </a>
     </li>
   );
+  }
 }
 
 function renderIcon(icon) {
   switch (icon) {
+    case 'home':
+      return(
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" fill="white" clip-rule="evenodd"><path d="M22 11.414v12.586h-20v-12.586l-1.293 1.293-.707-.707 12-12 12 12-.707.707-1.293-1.293zm-6 11.586h5v-12.586l-9-9-9 9v12.586h5v-9h8v9zm-1-7.889h-6v7.778h6v-7.778z"/></svg>
+      );
     case 'about':
       return (
         <svg width="24" className='lg:hidden' height="24" xmlns="http://www.w3.org/2000/svg" fill="white" fill-rule="evenodd" clip-rule="evenodd" aria-hidden='true'><path d="M12 0c-5.083 0-8.465 4.949-3.733 13.678 1.596 2.945-1.725 3.641-5.09 4.418-3.073.709-3.187 2.235-3.177 4.904l.004 1h23.99l.004-.969c.012-2.688-.093-4.223-3.177-4.935-3.438-.794-6.639-1.49-5.09-4.418 4.719-8.912 1.251-13.678-3.731-13.678m0 1c1.89 0 3.39.764 4.225 2.15 1.354 2.251.866 5.824-1.377 10.06-.577 1.092-.673 2.078-.283 2.932.937 2.049 4.758 2.632 6.032 2.928 2.303.534 2.412 1.313 2.401 3.93h-21.998c-.01-2.615.09-3.396 2.401-3.93 1.157-.266 5.138-.919 6.049-2.94.387-.858.284-1.843-.304-2.929-2.231-4.115-2.744-7.764-1.405-10.012.84-1.412 2.353-2.189 4.259-2.189"/></svg>
